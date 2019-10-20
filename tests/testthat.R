e510bf4554b2a3407fef1606726cf45cccc0810a
Pierrick Roger <pk.roger@icloud.com>
2019-10-20 19:33:31 +0200

    Add package file.

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,6 @@
 # Script needed to run testthat automatically from ‘R CMD check’. See
 # testthat::test_dir documentation.
 library(testthat)
-library(biodb)
+library(biodbExpasy)
 Sys.setenv(TESTTHAT_REPORTER = "summary")
-test_check("biodb")
+test_check("biodbExpasy")
